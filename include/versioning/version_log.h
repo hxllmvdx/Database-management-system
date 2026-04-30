d6b931467b787e09424c70208a1d27fbc572b9c5
hxllmvdx <jigoku.tyan@gmail.com>
2026-04-30 11:42:09 +0300

    implemented version logger

--- include/versioning/version_log.h
+++ include/versioning/version_log.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <fstream>
 #include <string>
 #include <vector>
+
+#include "../common/bytes.h"
 #include "../common/status.h"
 #include "version_record.h"
 
@@ -11,11 +14,23 @@ public:
     explicit VersionLog(std::string file_path);
 
     Status Open();
+    Status Flush();
     Status Append(const VersionRecord& record);
     Status ReadAll(std::vector<VersionRecord>* out);
 
+    const std::string& file_path() const { return file_path_; }
+
 private:
+    Status InitializeLogFile();
+    Status ValidateLogHeader();
+    Status WriteLogHeader();
+    Status ValidateRecord(const VersionRecord& record) const;
+    Status SerializeRecord(const VersionRecord& record, ByteBuffer* out) const;
+    Status DeserializeRecord(const ByteBuffer& bytes, std::size_t* offset, VersionRecord* out) const;
+
     std::string file_path_;
+    std::fstream file_;
+    bool is_open_ = false;
 };
 
 }
