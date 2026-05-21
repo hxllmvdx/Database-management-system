8975eb2e7269861feb0e8fdacdb6f4cd14005bbe
kerbzyxerr <ars231106@gmail.com>
2026-05-21 14:11:10 +0300

    Add database lifecycle adapter

--- src/server/database.cpp
+++ src/server/database.cpp
@@ -1,1 +1,17 @@
+#include "server/database.h"
 
+#include <utility>
+
+db::Database::Database(Config config) : engine_(std::move(config)) {}
+
+db::Status db::Database::Start() {
+    return engine_.Start();
+}
+
+db::Status db::Database::Stop() {
+    return engine_.Stop();
+}
+
+db::StorageNodeEngine& db::Database::engine() {
+    return engine_;
+}
